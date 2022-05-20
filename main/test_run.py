340f24ea49a6ea2fba05465b73940d0b1b000657
Nitro <admin@nitrostudio.dev>
2022-05-20 12:00:53 -0700

    FPS counter added.

--- main/test_run.py
+++ main/test_run.py
@@ -1,4 +1,6 @@
 import cv2
+import time
+from matplotlib import image
 import numpy as np
 import image_processing as ip
 
@@ -8,7 +10,7 @@ if not cap.isOpened():
     print("Cannot open camera")
     exit()
 
-
+last_time = time.time()
 while True:
     ret, image_org = cap.read()
 
@@ -18,8 +20,17 @@ while True:
 
 
     # Our operations on the frame come here
-    image = ip.face_detection(image_org)
+    # image = ip.face_detection(image_org)
+    # image = ip.to_gray(image_org)
 
+
+
+    # FPS Counter
+    current_time = time.time()
+    fps = 1 / (current_time - last_time)
+    last_time = current_time
+
+    cv2.putText(image, f'FPS: {int(fps)}', (50, 50), cv2.FONT_HERSHEY_PLAIN, 3, (161, 252, 3), 3)
 
     # Display the resulting frame
     cv2.imshow('image', image)
